21a685016314aafb94b301c6d60de847a8603417
Jens Wissmann <jens.wissmann@gmail.com>
2014-10-27 00:23:43 +0100

    added analysis script and code book

---
+++ getdata-008/run_analysis/run_analysis.R
@@ -0,0 +1,87 @@
+##
+# -1. Install and require package dependencies
+
+# Require a package. Install package if not yet installed.
+# (Adapted from BondedDust's answer on http://stackoverflow.com/a/19843463/2392982)
+insist <- function(pkg){
+         if ( !require(pkg, character.only=TRUE) ) {
+              install.packages(as.character(pkg), dependencies=TRUE)
+              require(pkg, character.only=TRUE) }}
+
+# require (and maybe install) depencendy
+insist ("reshape2")
+#(insist "data.table")
+
+##
+# 0. Download dataset and read relevant data
+
+# location for local temporary file
+dataZipFile <- "/tmp/FUCI_HAR_Dataset.zip"
+# download data iff not done yet
+if(!file.exists(dataZipFile)) {
+  download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", dataZipFile)
+}
+
+# Unzip and read training data.
+
+subject_train <- read.table(unz(dataZipFile, 'UCI HAR Dataset/train/subject_train.txt'))
+x_train       <- read.table(unz(dataZipFile, 'UCI HAR Dataset/train/X_train.txt'))
+y_train       <- read.table(unz(dataZipFile, 'UCI HAR Dataset/train/y_train.txt'))
+
+# Unzip and read test data.
+
+subject_test  <- read.table(unz(dataZipFile, 'UCI HAR Dataset/test/subject_test.txt'))
+x_test        <- read.table(unz(dataZipFile, 'UCI HAR Dataset/test/X_test.txt'))
+y_test        <- read.table(unz(dataZipFile, 'UCI HAR Dataset/test/y_test.txt'))
+
+# Unzip and read features
+features       <- read.table(unz(dataZipFile, "UCI HAR Dataset/features.txt"), header=F, colClasses="character")
+
+# Unzip and read activities
+activities <- read.table(unz(dataZipFile, "UCI HAR Dataset/activity_labels.txt"), header=F, colClasses="character")
+
+##
+# 1.  Merge the training and the test sets to create one data set.
+
+subject_data <- rbind(subject_train, subject_test)
+x_data       <- rbind(x_train, x_test)
+y_data       <- rbind(y_train, y_test)
+
+data <- list(x=subject_data, y=x_data, subject=subject_data)
+
+##
+# 2. Extract the measurements on the mean and standard deviation for each measurement.
+
+# identify feature entries for mean and standard deviation
+is_mean_or_std     <- grepl("mean|std", features[,2])
+extracted_features <- features[is_mean_or_std,2]
+
+# extract features from dataset x
+extracted_data           <- data$x[, is_mean_or_std]
+colnames(extracted_data) <- extracted_features
+
+# reduce data for x
+data$x <- extracted_data
+
+##
+# 3. Use descriptive activity namesm and appropriate labels with descriptive variable names.
+
+colnames(data$y) <- "Activity"
+data$y$activity[data$y$activity == 1] = "WALKING"
+data$y$activity[data$y$activity == 2] = "WALKING_UPSTAIRS"
+data$y$activity[data$y$activity == 3] = "WALKING_DOWNSTAIRS"
+data$y$activity[data$y$activity == 4] = "SITTING"
+data$y$activity[data$y$activity == 5] = "STANDING"
+data$y$activity[data$y$activity == 6] = "LAYING"
+
+colnames(data$subject) <- c("Subject")
+
+##
+# 4. Create the tidy data set.
+
+df <- bind.data(data$x, data$y, data$subject)
+
+# Create tidy dataset
+tidy_data <- create.tidy.dataset(combined)
+
+write.csv(tidy, "tidy_data.txt", row.names=FALSE)
